203b5b8548b610148413e0e531c19187dc9a8279
LiceyMaxim <maks.semyonov.2004@gmail.com>
2021-10-08 03:37:54 +0300

    fix
    function takes a list of strings and casts to the set, fixed unittests

--- models/sorensen/test.py
+++ models/sorensen/test.py
@@ -5,24 +5,22 @@ from sorensens_dice import comparison
 class TestComparison(unittest.TestCase):
 
     def test_positive(self):
-        self.assertEqual(comparison('sun', 'sun'), 1.0)
-        self.assertEqual(comparison('', ''), 1.0)
-        self.assertEqual(comparison('abc', 'cba'), 1.0)
-        self.assertEqual(comparison('sunflower', 'sun'), 0.5)
-        self.assertEqual(comparison('sun sun sun sun', 'sun'), 0.3333333333333333)
-        self.assertEqual(comparison('abc', 'mnl'), 0.0)
-
-    def test_types(self):
-        self.assertRaises(TypeError, comparison, 42)
-        self.assertRaises(TypeError, comparison, True)
-        self.assertRaises(TypeError, comparison, ['59.3333333', '12.5'])
-        self.assertRaises(TypeError, comparison, 3 + 4j)
-        self.assertRaises(TypeError, comparison, {1: 2})
-        self.assertRaises(TypeError, comparison, (3, 4))
+        self.assertEqual(comparison(['sun'], ['sun']), 1.0)
+        self.assertEqual(comparison(['a', '+', 'b'], ['b', '+', 'a']), 1.0)
+        self.assertEqual(comparison(['int', 'input', '()'], ['int', 'input', '(":")']), 2/3)
+        self.assertEqual(comparison(['int', 'input', '()'], ['str', 'input', '(":")']), 1/3)
+        self.assertEqual(comparison(['_init_'], ['__init__']), 0.0)
 
     def test_negative(self):
-        self.assertNotEqual(comparison('sunsun', 'sun'), 1.0)
-        self.assertNotEqual(comparison('nsu', 'sun'), 0.0)
-        self.assertNotEqual(comparison('ssuunn', 'sun'), 1.0)
-        self.assertNotEqual(comparison('s u n', 'sun'), 1.0)
-        self.assertNotEqual(comparison('abc', 'def'), 1.0)
+        self.assertNotEqual(comparison(['openspace'], ['open_space']), 1.0)
+        self.assertNotEqual(comparison(['openspace'], ['open', 'space']), 1.0)
+        self.assertNotEqual(comparison(['openspace'], ['"openspace"']), 1.0)
+        self.assertNotEqual(comparison(['openspace'], [' openspace']), 1.0)
+        self.assertNotEqual(comparison(['openspace'], ["openspace"]), 0.0)
+
+    def test_types(self):
+        self.assertRaises(TypeError, comparison, 42, 15)
+        self.assertRaises(TypeError, comparison, True, True)
+        self.assertRaises(TypeError, comparison, '3', [25])
+        self.assertRaises(TypeError, comparison, (1, 2), ["25"])
+        self.assertRaises(TypeError, comparison, [3], '25')
