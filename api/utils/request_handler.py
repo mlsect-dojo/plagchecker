8c92670fc2d6216aba3691467a706d712f15bb67
theo <theo.kossoy@gmail.com>
2021-10-10 12:51:35 +0300

    Rename handler to request_handler

--- api/utils/request_handler.py
+++ api/utils/request_handler.py
@@ -1,29 +1,30 @@
-import pathlib
+from pathlib import Path
 import uuid
 
 import aiofiles
 import aiofiles.os
 from fastapi.datastructures import UploadFile
 
-from api.utils.checks import Checks
 from api.utils.sqliteconnector import SQLiteConnector
+from api.utils.checks import Jaccard, Levenshtein
 
 
-class Handler():
+class RequestHandler():
 
     def __init__(self) -> None:
         self.connector = SQLiteConnector()
-        self.checks = Checks()
+        self.levenshtein = Levenshtein()
+        self.jaccard = Jaccard()
         self.methods = [
-            (self.checks.levenshtein_check, False),
-            (self.checks.jaccard_check, True)
+            (self.levenshtein, False),
+            (self.jaccard, True)
         ]
 
     async def save_lab(self, user_id: int, ext: str, archive: UploadFile) -> int:
         filename = f'{uuid.uuid4()}.zip'
         data = archive.file.read()
         
-        async with aiofiles.open(f'{pathlib.Path(__file__).parent.resolve()}/api/labs/{filename}', 'wb') as file:
+        async with aiofiles.open(f'{Path(__file__).parent.resolve()}/api/labs/{filename}', 'wb') as file:
             await file.write(data)
 
         lab_id = await self.connector.insert_lab(f'{filename}', user_id, ext)
@@ -33,19 +34,19 @@ class Handler():
         path = await self.connector.delete_lab(lab_id)
 
         if path:
-            await aiofiles.os.remove(f'{pathlib.Path(__file__).parent.resolve()}/api/labs/{path}')
+            await aiofiles.os.remove(f'{Path(__file__).parent.resolve()}/api/labs/{path}')
 
     async def lab_score_levenshtein(self, lab_id: int, limit: int = None) -> dict:
-        db_result = await self.connector.get_lab_score(lab_id, 'levenshtein_check')
+        db_result = await self.connector.get_lab_score(lab_id, self.levenshtein.name())
         
         if db_result != []:
             result_sorted = sorted(db_result, key = lambda k: k['score'], reverse = False)
             levenshtein_score = [{'id': result['id'], 'score': result['score']} for result in result_sorted]
         else:
-            check_result = await self.checks.levenshtein_check(lab_id)
+            check_result = await self.levenshtein.check(lab_id)
             levenshtein_score = sorted(check_result, key = lambda k: k['score'], reverse = False)
             results = [(item['id'], item['score']) for item in levenshtein_score]
-            await self.connector.save_lab_score(lab_id, self.checks.levenshtein_check.__name__, results)
+            await self.connector.save_lab_score(lab_id, self.levenshtein.name(), results)
 
         if not limit:
             limit = 10
@@ -56,22 +57,22 @@ class Handler():
         result = {'similars': []}
         
         for algorithm, reverse in self.methods:
-            db_result = await self.connector.get_lab_score(lab_id, algorithm.__name__)
+            db_result = await self.connector.get_lab_score(lab_id, algorithm.name())
 
             if db_result != []:
                 result_sorted = sorted(db_result, key = lambda k: k['score'], reverse = reverse)
                 algorithm_result = [{'id': result['id'], 'score': result['score']} for result in result_sorted]
             else:
-                check_result = await algorithm(lab_id)
+                check_result = await algorithm.check(lab_id)
                 algorithm_result = sorted(check_result, key = lambda k: k['score'], reverse = reverse)
                 results = [(item['id'], item['score']) for item in algorithm_result]
-                await self.connector.save_lab_score(lab_id, algorithm.__name__, results)
+                await self.connector.save_lab_score(lab_id, algorithm.name(), results)
             
             if not limit:
                 limit = 10
 
             result['similars'].append({
-                'algorithm': algorithm.__name__,
+                'algorithm': algorithm.name(),
                 'top': algorithm_result[:limit]
             })
         
--- server.py
+++ server.py
@@ -1,29 +1,29 @@
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 
-from api.utils.handler import Handler
+from api.utils.request_handler import RequestHandler
 
 server = FastAPI()
-handler = Handler()
+request_handler = RequestHandler()
 
 @server.put('/lab')
 async def save_lab(user_id: int, ext: str, archive: UploadFile = File(...)):
-    lab_id = await handler.save_lab(user_id, ext, archive)
+    lab_id = await request_handler.save_lab(user_id, ext, archive)
     return {'id': lab_id}
 
 @server.delete('/lab')
 async def delete_lab(lab_id: int):
-    await handler.delete_lab(lab_id)
+    await request_handler.delete_lab(lab_id)
     return {'status': 'OK'}
 
 @server.get('/score/all')
 async def lab_score_all(lab_id: int, limit: int = None):
-    result = await handler.lab_score_all(lab_id, limit)
+    result = await request_handler.lab_score_all(lab_id, limit)
     return result
 
 @server.get('/score/levenshtein')
 async def lab_score_levenshtein(lab_id: int, limit: int = None):
-    similars = await handler.lab_score_levenshtein(lab_id, limit)
+    similars = await request_handler.lab_score_levenshtein(lab_id, limit)
     return similars
 
 if __name__ == '__main__':
